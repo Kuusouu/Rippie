14dfb35311bbe5699ae421da63fec75798eea55b
Omar <90123670+omarnunezsiri@users.noreply.github.com>
2026-06-28 02:12:47 -0400

    fix: remove duplicate code

--- src/client.d.ts
+++ src/client.d.ts
@@ -3,11 +3,6 @@ import { Collection } from 'discord.js';
 // Module augmentation, commands are available within the Discord Client class without a explicit cast or
 // separate class
 declare module 'discord.js' {
-	interface GuildSettings {
-		musicChannelId?: string;
-		services?: Record<string, boolean>;
-	}
-
 	interface Client<Ready extends boolean = boolean> {
 		commands: Collection<string, any>;
 		settings: Record<string, GuildSettings>;
