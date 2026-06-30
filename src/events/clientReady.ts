e54f163249436dc19fbc73671aec201aeb2e4f0a
Omar <90123670+omarnunezsiri@users.noreply.github.com>
2026-06-30 05:57:27 -0400

    refactor: extract event handlers to separate files

---
+++ src/events/clientReady.ts
@@ -0,0 +1,7 @@
+import { Client, Events } from 'discord.js';
+
+export const registerClientReadyHandler = (client: Client): void => {
+	client.once(Events.ClientReady, (readyClient: Client<true>) => {
+		console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	});
+};
